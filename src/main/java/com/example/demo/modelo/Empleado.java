8c75fb652831df8de0599c983017fa666ca177de
Fran-Jos <fjchanataxi@uce.edu.ec>
2023-07-23 22:31:21 -0500

    creación de tablas Emp , atenMedica, vete, con sus respectivas relaciones

---
+++ src/main/java/com/example/demo/modelo/Empleado.java
@@ -0,0 +1,137 @@
+package com.example.demo.modelo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empleado")
+public class Empleado {
+	
+	@Id
+	@Column(name = "empl_id")
+	private Integer id;
+	
+	@Column(name = "empl_cedula_pasaporte")
+	private String cedula_pasaporte;
+	
+	@Column(name = "empl_nombre")
+	private String nombre;
+	
+	@Column(name = "empl_apellido")
+	private String apellido;
+	
+	@Column(name = "empl_fecha_nacimiento")
+	private String fechaNacimiento;
+	
+	@Column(name = "empl_fecha_contrato")
+	private LocalDateTime fechaContrato;
+	
+	@Column(name = "empl_renovacion")
+	private LocalDateTime fechaRenovacion;
+	
+	@Column(name = "empl_tipo")
+	private String tipo;
+	
+	@Column(name = "empl_observacion")
+	private String observacion;
+	
+	@OneToMany(mappedBy = "empleado")
+	private List<AtencionMedica> atencionMedica;
+	
+	
+//	@OneToOne
+//	@JoinColumn(name="empleado_id_guia")
+//	private Guia guia;
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCedula_pasaporte() {
+		return cedula_pasaporte;
+	}
+
+	public void setCedula_pasaporte(String cedula_pasaporte) {
+		this.cedula_pasaporte = cedula_pasaporte;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public LocalDateTime getFechaContrato() {
+		return fechaContrato;
+	}
+
+	public void setFechaContrato(LocalDateTime fechaContrato) {
+		this.fechaContrato = fechaContrato;
+	}
+
+	public LocalDateTime getFechaRenovacion() {
+		return fechaRenovacion;
+	}
+
+	public void setFechaRenovacion(LocalDateTime fechaRenovacion) {
+		this.fechaRenovacion = fechaRenovacion;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public List<AtencionMedica> getAtencionMedica() {
+		return atencionMedica;
+	}
+
+	public void setAtencionMedica(List<AtencionMedica> atencionMedica) {
+		this.atencionMedica = atencionMedica;
+	}
+	
+
+
+}
---
+++ src/main/java/com/example/demo/modelo/Veterinario.java
@@ -0,0 +1,122 @@
+package com.example.demo.modelo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Table(name = "veterinario")
+@Entity
+public class Veterinario {
+	
+	//@GeneratedValue(generator = "seq_vete" , strategy =  GenerationType.SEQUENCE)
+	//@SequenceGenerator(name = "seq_vete" ,sequenceName = "seq_vete",allocationSize = 1)
+	@Column(name = "vete_id")
+	@Id
+	private Integer id;
+	
+	
+	@Column(name = "vete_nombre")
+	private String nombre;
+	
+	
+	@Column(name = "vete_apellido")
+	private String apellido;
+	
+	
+	@Column(name = "vete_cedula_pasaporte")
+	private String cedula_pasaporte;
+	
+	
+	@Column(name = "vete_fechaNacimiento")
+	private LocalDateTime fechaNacimiento;
+	
+	
+	@Column(name = "vete_direccion")
+	private String direccion;
+	
+	
+	@Column(name = "vete_especialidad")
+	private String especialidad;
+	
+	@OneToMany(mappedBy = "veterinario" )
+	private List<AtencionMedica> atencionMedica;
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula_pasaporte() {
+		return cedula_pasaporte;
+	}
+
+	public void setCedula_pasaporte(String cedula_pasaporte) {
+		this.cedula_pasaporte = cedula_pasaporte;
+	}
+
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public List<AtencionMedica> getAtencionMedica() {
+		return atencionMedica;
+	}
+
+	public void setAtencionMedica(List<AtencionMedica> atencionMedica) {
+		this.atencionMedica = atencionMedica;
+	}
+	
+	
+	
+
+
+}
